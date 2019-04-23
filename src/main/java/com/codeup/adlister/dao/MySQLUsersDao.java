dceefea5414f299661fc96b1cf9479c49eafdfc7
Cory Musick <cory.musick37@gmail.com>
2019-04-23 14:03:41 -0500

    initial set up. added config file

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,8 +1,8 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
-
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
